88ef9de6592347cd402eac4fa80bb7e5a9026566
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2019-05-17 07:15:22 -0700

    allow user configs without custom image

---
+++ src/scripts/startup/link_user_configs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+SOURCE_DIR=/etc/nginx/user.conf.d
+TARGET_DIR=/etc/nginx/nginx.conf.d
+
+echo "symlinking scripts from ${SOURCE_DIR} to ${TARGET_DIR}"
+
+if [ ! -d "$SOURCE_DIR" ]; then
+    echo "no ${SOURCE_DIR}, nothing to do."
+else
+    for conf in ${SOURCE_DIR}/*.conf; do
+        echo "symlinking ${conf} to ${TARGET_DIR}"
+        ln -s ${SOURCE_DIR}$/{conf} ${TARGET_DIR}
+    done
+fi
